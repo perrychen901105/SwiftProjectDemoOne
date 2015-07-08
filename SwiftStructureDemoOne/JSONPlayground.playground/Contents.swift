8a62339ca157310750ed0a8b09e873ffb9025cc7
Perry <chenzhipeng901105@gmail.com>
2015-07-08 10:59:09 +0800

    add

---
+++ SwiftStructureDemoOne/JSONPlayground.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+    {
+        "result": "OK",
+        "errorCode": 0,
+        "errorDescription": "",
+        "body": {
+            "employees": [
+            { "firstName":"John" , "lastName":"Doe" },
+            { "firstName":"Anna" , "lastName":"Smith" },
+            { "firstName":"Peter" , "lastName":"Jones" }
+            ]
+        }
+    }
